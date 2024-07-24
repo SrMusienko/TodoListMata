3d4edd4e7eda3ab783b95eb3903dd8200f5d0f4d
Sergii Musiienko <vpnmus@ukr.net>
2024-07-24 14:59:50 +0300

    add all functionality for Tag and customize view

--- todo/views.py
+++ todo/views.py
@@ -1,55 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
-def index(request):
-    """View function for the home page of the site."""
+def mark(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.done = not task.done
+    task.save()
 
-    num_drivers = 0
-    num_cars = 0
-    num_manufacturers = 9
-
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-
-    context = {
-        "num_drivers": num_drivers,
-        "num_cars": num_cars,
-        "num_manufacturers": num_manufacturers,
-        "num_visits": num_visits + 1,
-    }
-
-    return render(request, "todo/index.html", context=context)
+    return redirect('task-list')
 
 
 class TaskListView(generic.ListView):
     model = Task
     context_object_name = "task_list"
     template_name = "todo/task_list.html"
-    paginate_by = 5
+    paginate_by = 3
     queryset = Task.objects.prefetch_related("tags")
 
 
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
-    success_url = reverse_lazy("task")
+    success_url = reverse_lazy("task-list")
     template_name = "todo/task_form.html"
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
     fields = "__all__"
-    success_url = reverse_lazy("task")
+    success_url = reverse_lazy("task-list")
     template_name = "todo/task_form.html"
 
 
 class TaskDeleteView(generic.DeleteView):
     model = Task
-    success_url = reverse_lazy("task:position-list")
+    success_url = reverse_lazy("task-list")
     template_name = "todo/task_confirm_delete.html"
 
 
+# --------------------Tag-------------------
+
+class TagListView(generic.ListView):
+    model = Tag
+    context_object_name = "tag_list"
+    template_name = "todo/tag_list.html"
+    paginate_by = 5
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tag-list")
+    template_name = "todo/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tag-list")
+    template_name = "todo/task_form.html"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("tag-list")
+    template_name = "todo/tag_confirm_delete.html"
