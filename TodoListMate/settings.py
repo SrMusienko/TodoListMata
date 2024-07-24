d86d9b9337f90bd87d98bf93de6c77d323c8c95d
Sergii Musiienko <vpnmus@ukr.net>
2024-07-24 11:19:52 +0300

    add includes for base.html and setup static

--- TodoListMate/settings.py
+++ TodoListMate/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
