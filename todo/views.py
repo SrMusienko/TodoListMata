9e9458f41790b52bd4b94c8874a1e3c6939205c0
Sergii Musiienko <vpnmus@ukr.net>
2024-07-24 11:34:15 +0300

    create draft of index.html

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_drivers = 0
+    num_cars = 0
+    num_manufacturers = 9
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_drivers": num_drivers,
+        "num_cars": num_cars,
+        "num_manufacturers": num_manufacturers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "todo/index.html", context=context)
+
